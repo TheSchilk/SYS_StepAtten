d1b5f6ed08b7bb776d914bb2c1d69475141ec5ad
TheSchilk <schilk.philipp@gmail.com>
2020-04-23 16:18:13 +0200

    Removed FPU warning

--- Firmware/Src/main.c
+++ Firmware/Src/main.c
@@ -1,6 +1,5 @@
-#if !defined(__SOFT_FP__) && defined(__ARM_FP)
-  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
-#endif
+#include <sys_stepatten.h>
+#include <stm32f0xx.h>
 
 #include <stm32f0xx.h>
 int main(void)
